e7c9aa5a4b138adc8162b1ccdfdcef4b7ffbf7f7
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-11-05 12:21:15 +0200

    feat: Initialize Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="site">
       <Navigation />
       <section className="main">
-        {/* <Routes>
-          <Route path="/about" />
-          <Route path="/contacts" />
-          <Route path="/projects" />
-        </Routes> */}
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/projects" /> */}
+          {/* <Route path="/about" /> */}
+        </Routes>
       </section>
     </div>
   );
