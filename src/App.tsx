141f1fd7def4a811d954e30d71c4966f8b0eb536
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-11-08 09:37:34 +0200

    app: reset application - delete all

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navigation from "./components/Navigation/Navigation";
-import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="site">
-      <Navigation />
       <section className="main">
         <Routes>
-          <Route path="/home" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           {/* <Route path="/projects" /> */}
           {/* <Route path="/about" /> */}
         </Routes>
