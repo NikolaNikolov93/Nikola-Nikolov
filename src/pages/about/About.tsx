6dc068815988b998fd8fb8015448527906a4fd22
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-11-17 14:28:57 +0200

    refactor: refactored About page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,18 +1,21 @@
+import useFetchCertificates from "../../lib/hooks/useFetchCertificates";
+import { Wrapper } from "./About.styles";
+import CertificateCard from "./CertificateCard";
+
 const About: React.FC = () => {
+  const courses = useFetchCertificates();
+
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui asperiores,
-      sed dicta voluptates minus eos maxime quis libero quod expedita autem rem,
-      consequatur tempora in voluptas! Repellendus, quibusdam facere? Quod
-      voluptatum repellat iure dolores vero possimus cumque illo, consectetur
-      dignissimos aspernatur error perferendis animi doloremque consequuntur
-      tempore at id, harum dicta culpa veniam, ratione autem vitae! Omnis hic
-      animi recusandae ipsum? Numquam eum nulla maxime, porro exercitationem
-      recusandae? Qui corrupti tempora nemo molestiae optio ab, temporibus illo!
-      Mollitia vero quo totam sequi obcaecati quia fugit aliquid facilis
-      pariatur, incidunt, distinctio veniam asperiores sint ea accusamus,
-      quisquam ipsa impedit doloribus iure.
-    </div>
+    <Wrapper>
+      {courses.map((course) => (
+        <CertificateCard
+          key={course.id}
+          id={course.id}
+          name={course.name}
+          url={course.certificateImg}
+        />
+      ))}
+    </Wrapper>
   );
 };
 
