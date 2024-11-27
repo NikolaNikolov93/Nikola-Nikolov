825ad02845adfeba7941c58eae41241c041c8c56
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-11-27 09:50:29 +0200

    refactor: separete theme context and theme context provider

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,5 +1,5 @@
 //React imports
-import { createContext, ReactNode, useState } from "react";
+import { createContext } from "react";
 import { ThemeContextType } from "../lib/types/types";
 
 //Create context
@@ -7,14 +7,3 @@ export const ThemeContext = createContext<ThemeContextType | undefined>(
   undefined
 );
 //Create context provider
-export const ThemeContextProvider = ({ children }: { children: ReactNode }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(true);
-  const toggleTheme = () => setIsDarkTheme(!isDarkTheme);
-
-  //Return the ThemeContext.Provider with the ThemeContext properties
-  return (
-    <ThemeContext.Provider value={{ isDarkTheme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
-};
---
+++ src/context/ThemeContextProvide.tsx
@@ -0,0 +1,13 @@
+import { ReactNode, useState } from "react";
+import { ThemeContext } from "./ThemeContext";
+export const ThemeContextProvider = ({ children }: { children: ReactNode }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(true);
+  const toggleTheme = () => setIsDarkTheme(!isDarkTheme);
+
+  //Return the ThemeContext.Provider with the ThemeContext properties
+  return (
+    <ThemeContext.Provider value={{ isDarkTheme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
